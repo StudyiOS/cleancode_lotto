5b3bd3bd99e10cfd273da0e35f1fe8fe8b688961
Leo <brightspread.jo@gmail.com>
2023-12-09 15:28:27 +0900

    🏗️ MVVM folders, Model defined #3
    - 폴더 생성
    - 모델 정의

---
+++ CleanLotto/CleanLotto/Model/Lotto.swift
@@ -0,0 +1,15 @@
+//
+//  Lotto.swift
+//  CleanLotto
+//
+//  Created by Leo on 12/9/23.
+//
+
+import Foundation
+
+struct WinningLotto {
+    let round: Int
+    let numbers: [Int]
+    let date: Date
+    let amount: Int
+}
---
+++ CleanLotto/CleanLotto/Views/Main/MainView/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  CleanLotto
+//
+//  Created by Leo on 12/9/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Hello Lotto!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
