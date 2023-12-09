5a994d2d01b158018ee23a1104b0996fbc7fb31c
Leo <brightspread.jo@gmail.com>
2023-12-09 11:37:45 +0900

    🎉 Initial code added
    - 프로젝트 세팅

---
+++ CleanLotto/CleanLotto/CleanLottoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanLottoApp.swift
+//  CleanLotto
+//
+//  Created by Leo on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanLottoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
