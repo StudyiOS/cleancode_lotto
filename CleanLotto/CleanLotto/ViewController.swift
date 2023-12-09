f8f92dc3095c3b3f8ddb0825b0779b7fd185460f
Jimmy <joony300@gmail.com>
2023-12-10 00:41:44 +0900

    [init] Project

---
+++ CleanLotto/CleanLotto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanLotto
+//
+//  Created by 정준영 on 12/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
